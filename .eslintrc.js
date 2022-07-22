01159fb22f8ce7c90b7eb82ce54eb01198e14e0e
Miki <mikcat@users.noreply.github.com>
2022-07-22 20:47:41 +0200

    eslint imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,39 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es2021: true,
+    es2022: true,
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     parser: 'babel-eslint',
-    ecmaVersion: 12,
+    ecmaVersion: 13,
     sourceType: 'module',
   },
   extends: [
     'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/react',
     'plugin:react/recommended',
-    'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'import', 'jsx-a11y'],
   // add your custom rules here
   rules: {
     'react/prop-types': 1,
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
   },
 };
