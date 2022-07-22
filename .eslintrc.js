52bf7d2fc984af6b580f46a8eea05b7d9f55f74a
Miki <mikcat@users.noreply.github.com>
2022-07-22 20:51:39 +0200

    eslint ignore html files

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
   },
+  ignorePatterns: ['**/*.html'],
   settings: {
     react: {
       version: 'detect',
